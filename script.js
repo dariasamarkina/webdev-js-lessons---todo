77d74c2cce271bbb03201770aeb7cb7ad308a407
DARIA S <DARIA@EXAMPLE.COM>
2023-06-01 21:33:12 +0300

    add api module

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
+import { addTodo, deleteTodo, getTodos } from "./api.js";
+
 let tasks = [];
 
 let token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
 token = null;
 
-const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
+
 
     const fetchTodosAndRender = () => {
-      return fetch(host, {
-        method: "GET",
-        headers: {
-          Authorization: token,
-        },
-      })
-        .then((response) => {
-          if (response.status === 401) {
-            // token = prompt("Введите верный пароль");
-            // fetchTodosAndRender();
-            throw new Error("Нет авторизации");
-          }
-
-          return response.json();
-        })
+      return getTodos({ token })
         .then((responseData) => {
           tasks = responseData.todos;
           renderApp();
@@ -108,15 +96,7 @@ const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
           const id = deleteButton.dataset.id;
 
           // подписываемся на успешное завершение запроса с помощью then
-          fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
-            method: "DELETE",
-            headers: {
-              Authorization: token,
-            },
-          })
-            .then((response) => {
-              return response.json();
-            })
+          deleteTodo({ id, token })
             .then((responseData) => {
               // получили данные и рендерим их в приложении
               tasks = responseData.todos;
@@ -133,38 +113,29 @@ const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
         buttonElement.disabled = true;
         buttonElement.textContent = "Задача добавляеятся...";
 
-        // подписываемся на успешное завершение запроса с помощью then
-        fetch(host, {
-          method: "POST",
-          body: JSON.stringify({
-            text: textInputElement.value,
-          }),
-          headers: {
-            Authorization: token,
-          },
+        addTodo({
+          text: textInputElement.value,
+          token
         })
-          .then((response) => {
-            return response.json();
-          })
-          .then(() => {
-            // TODO: кинуть исключение
-            textInputElement.value = "";
-          })
-          .then(() => {
-            return fetchTodosAndRender();
-          })
-          .then(() => {
-            buttonElement.disabled = false;
-            buttonElement.textContent = "Добавить";
-          })
-          .catch((error) => {
-            console.error(error);
-            alert("Кажется у вас проблемы с интернетом, попробуйте позже");
-            buttonElement.disabled = false;
-            buttonElement.textContent = "Добавить";
-          });
-      });
-    };
+        .then(() => {
+          // TODO: кинуть исключение
+          textInputElement.value = "";
+        })
+        .then(() => {
+          return fetchTodosAndRender();
+        })
+        .then(() => {
+          buttonElement.disabled = false;
+          buttonElement.textContent = "Добавить";
+        })
+        .catch((error) => {
+          console.error(error);
+          alert("Кажется у вас проблемы с интернетом, попробуйте позже");
+          buttonElement.disabled = false;
+          buttonElement.textContent = "Добавить";
+        });
+    });
+  };
 
     // fetchTodosAndRender();
     renderApp();
