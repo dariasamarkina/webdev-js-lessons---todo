b45cab9a295ba0b3cbc73025b83ae6a8659940ee
DARIA S <DARIA@EXAMPLE.COM>
2023-06-01 21:10:32 +0300

    add dynamic login form

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let tasks = [];
 
 let token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+token = null;
 
 const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
 
@@ -27,7 +28,37 @@ const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
     };
 
     const renderApp = () => {
+
       const appEl = document.getElementById("app");
+
+      if (!token) {
+        const appHtml = `
+                <h1>Список задач</h1>
+                <div class="form">
+                <h3 class="form-title">Форма входа</h3>
+                <div class="form-row">
+                    Логин
+                    <input type="text" id="login-input" class="input" />
+                    <br />
+                    Пароль
+                    <input type="text" id="login-input" class="input" />
+                </div>
+                <br />
+                <button class="button" id="login-button">Войти</button>
+                </div>`;
+
+        appEl.innerHTML = appHtml;
+
+        
+        document.getElementById('login-button').addEventListener('click', () => {
+          token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+          fetchTodosAndRender();
+        })
+
+        return;
+      }
+
+
       const tasksHtml = tasks
         .map((task) => {
           return `
@@ -42,19 +73,6 @@ const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
 
       const appHtml = `
                 <h1>Список задач</h1>
-                <div class="form">
-                <h3 class="form-title">Форма входа</h3>
-                <div class="form-row">
-                    Логин
-                    <input type="text" id="login-input" class="input" />
-                    <br />
-                    Пароль
-                    <input type="text" id="login-input" class="input" />
-                </div>
-                <br />
-                <button class="button" id="login-button">Войти</button>
-                </div>
-
                 <ul class="tasks" id="list">
                 <!-- Список рендерится из JS -->
                 ${tasksHtml}
@@ -148,5 +166,6 @@ const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
       });
     };
 
-    fetchTodosAndRender();
+    // fetchTodosAndRender();
+    renderApp();
 
