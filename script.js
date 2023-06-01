046f69d538d69e58bed64ab7cdcbf5b24a6ccadc
DARIA S <DARIA@EXAMPLE.COM>
2023-06-01 20:48:59 +0300

    render html in js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const buttonElement = document.getElementById("add-button");
-const listElement = document.getElementById("list");
-const textInputElement = document.getElementById("text-input");
-
 let tasks = [];
 
 const host = 'https://wedev-api.sky.pro/api/v2/todos';
@@ -29,19 +25,59 @@ const fetchTodosAndRender = () => {
 };
 
 const renderTasks = () => {
-  const tasksHtml = tasks
-    .map((task) => {
-      return `
-          <li class="task">
-            <p class="task-text">
-              ${task.text}
-              <button data-id="${task.id}" class="button delete-button">Удалить</button>
-            </p>
-          </li>`;
-    })
-    .join("");
 
-  listElement.innerHTML = tasksHtml;
+  const appEl = document.getElementById('app');
+
+  const tasksHtml = tasks
+  .map((task) => {
+    return `
+    <li class="task">
+      <p class="task-text">
+        ${task.text}
+        <button data-id="${task.id}" class="button delete-button">Удалить</button>
+      </p>
+    </li>`;
+  })
+  .join("");
+
+const appHtml = `
+<h1>Список задач</h1>
+
+<div class="form">
+    <h3 class="form-title">Форма входа</h3>
+    <div class="form-row">
+      Логин
+      <input type="text" id="login-input" class="input"/>
+      <br>
+      Пароль
+      <input type="text" id="password-input" class="input"/>
+    </div>
+    <br />
+    <button class="button" id="login-button">Войти</button>
+  </div>
+
+<ul class="tasks" id="list">
+${tasksHtml}
+</ul>
+<br />
+<div class="form">
+  <h3 class="form-title">Форма добавления</h3>
+  <div class="form-row">
+    Что нужно сделать:
+    <input type="text" id="text-input" class="input" placeholder="Выпить кофе" />
+  </div>
+  <br />
+  <button class="button" id="add-button">Добавить</button>
+</div>`
+
+
+
+  appEl.innerHTML = appHtml;
+
+  const buttonElement = document.getElementById("add-button");
+  const listElement = document.getElementById("list");
+  const textInputElement = document.getElementById("text-input");
+
   const deleteButtons = document.querySelectorAll(".delete-button");
 
   for (const deleteButton of deleteButtons) {
@@ -50,7 +86,7 @@ const renderTasks = () => {
 
       const id = deleteButton.dataset.id;
 
-      fetch("https://webdev-hw-api.vercel.app/api/v2/todos/" + id, {
+      fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
             method: "DELETE",
             headers: {
               Authorization: token,
@@ -60,51 +96,53 @@ const renderTasks = () => {
               return response.json();
             })
             .then((responseData) => {
+              // получили данные и рендерим их в приложении
               tasks = responseData.todos;
               renderTasks();
             });
         });
       }
+
+      buttonElement.addEventListener("click", () => {
+        if (textInputElement.value === "") {
+          return;
+        }
+      
+        buttonElement.disabled = true;
+        buttonElement.textContent = 'Элемент добавляется…';
+      
+        fetch(host, {
+              method: "POST",
+              body: JSON.stringify({
+                text: textInputElement.value,
+              }),
+              headers: {
+                Authorization: token,
+              },
+            })
+              .then((response) => {
+                return response.json();
+              })
+              .then(() => {
+                textInputElement.value = "";
+              })
+              .then(() => {
+                return fetchTodosAndRender();
+              })
+              .then(() => {
+                buttonElement.disabled = false;
+                buttonElement.textContent = "Добавить";
+              })
+              .catch((error) => {
+                console.error(error);
+                alert("Кажется, у вас проблемы с интернетом, попробуйте позже");
+                buttonElement.disabled = false;
+                buttonElement.textContent = "Добавить";
+              });
+      
+            renderTasks();
+          });
     };
 
 fetchTodosAndRender();
 
-buttonElement.addEventListener("click", () => {
-  if (textInputElement.value === "") {
-    return;
-  }
-
-  buttonElement.disabled = true;
-  buttonElement.textContent = 'Элемент добавляется…';
-
-  fetch(host, {
-        method: "POST",
-        body: JSON.stringify({
-          text: textInputElement.value,
-        }),
-        headers: {
-          Authorization: token,
-        },
-      })
-        .then((response) => {
-          return response.json();
-        })
-        .then(() => {
-          textInputElement.value = "";
-        })
-        .then(() => {
-          return fetchTodosAndRender();
-        })
-        .then(() => {
-          buttonElement.disabled = false;
-          buttonElement.textContent = "Добавить";
-        })
-        .catch((error) => {
-          console.error(error);
-          alert("Кажется, у вас проблемы с интернетом, попробуйте позже");
-          buttonElement.disabled = false;
-          buttonElement.textContent = "Добавить";
-        });
-
-      renderTasks();
-    });
